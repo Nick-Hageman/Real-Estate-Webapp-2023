fe61f358b7aed64babad455778726cd2e16b993c
Nick Hageman <nicholas-hageman@uiowa.edu>
2023-06-22 19:21:51 -0500

    refactor: Pivoting Hageman Homes website to use React.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-  config = require('./config/config');
-
-var app = express();
-
-require('./config/express')(app, config);
-
-console.log('Starting up on: ' + config.port);
-app.listen(config.port);

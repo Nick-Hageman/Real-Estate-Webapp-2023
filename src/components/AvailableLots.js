46fe9a721bab7ff0d28faf8a993cebdae2ca2c9a
Nick Hageman <nickhageman8@gmail.com>
2023-07-01 13:19:07 -0500

    fix: fixed iframe map to fit screen

--- src/components/AvailableLots.js
+++ src/components/AvailableLots.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-import LotData from './LotData';
 import '../LandingPage.css';
 
 export default function AvailableLots() {
     return (
-      <div className='lot-data-container'>
-        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1BTGHYW6WZXKU6vh-igcNvV2iWKuxkOo&ehbc=2E312F" className='lot-map'></iframe>
-        <LotData />
-      </div>
+      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1BTGHYW6WZXKU6vh-igcNvV2iWKuxkOo&ehbc=2E312F" className='lot-map'></iframe>
     );
 }

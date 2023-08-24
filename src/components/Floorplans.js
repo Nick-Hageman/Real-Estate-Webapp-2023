172db17c64c9e76cdcbb00ba7f46d20daae95ee8
Nick Hageman <nickhageman8@gmail.com>
2023-08-24 14:51:19 -0500

    temporarily hardcoded the JSON response because API takes too long on a cold start

--- src/components/Floorplans.js
+++ src/components/Floorplans.js
@@ -7,9 +7,10 @@ export default function Floorplans() { // going to map contents from CMS
 
 
   async function logData() {
-    const response = await fetch("https://house-card.onrender.com/api/housecards");
-    const information = await response.json();
-    setDataset(information.data);
+    //const response = await fetch("https://house-card.onrender.com/api/housecards");
+    const response = require('./sample.json'); // hard-coded JSON to save load time, don't want to pay for faster API.
+    //const information = await response.json();
+    setDataset(response.data);
   }
 
   useEffect(() => { // executes on load and fetches CMS data
